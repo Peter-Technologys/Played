f665e14905a8bab911324488d30089033c3860fe
Peter Suma <sumapeter94@gmail.com>
2026-07-14 14:16:45 +0000

    fix: import dart:isolate for IsolateSpawnException in my_space_provider.dart

--- lib/features/my_space/presentation/providers/my_space_provider.dart
+++ lib/features/my_space/presentation/providers/my_space_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:isolate';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/models/media_item.dart';
