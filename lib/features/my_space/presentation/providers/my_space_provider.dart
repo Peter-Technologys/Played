352768fd7e69d2f5d03b872ea1e95d5bc3e96834
Peter Ways <pways2365@gmail.com>
2026-07-17 18:15:13 +0000

    fix: restore missing EventChannel import in my_space_provider.dart
    Removing dart:isolate and the compute() top-level function also
    accidentally dropped the package:flutter/services.dart import that
    provides EventChannel. This caused a compile error:
    
      Error: Method not found: 'EventChannel'
    
    Re-adding the services.dart import restores EventChannel for the
    live MediaStore observer stream.

--- lib/features/my_space/presentation/providers/my_space_provider.dart
+++ lib/features/my_space/presentation/providers/my_space_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/models/media_item.dart';
 import '../../../../core/database/played_database.dart';
