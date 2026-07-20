40cbe0f71e1b34b6568850b26fcae3ddcb7bed3a
Peter Katabira <tryv2@petersmartlink.com>
2026-07-20 15:51:32 +0000

    feat(v1.4.0): add favorites Hive box constant

--- lib/core/database/hive_boxes.dart
+++ lib/core/database/hive_boxes.dart
@@ -4,4 +4,5 @@ abstract class HiveBoxes {
   static const String seekPositions = 'otya_seek_positions';
   static const String shelfCache    = 'otya_shelf_cache';
   static const String vault         = 'otya_vault'; // AES-256 encrypted metadata
+  static const String favorites     = 'otya_favorites';
 }
