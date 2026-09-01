2e6b8e3f95457294f984a05d5ca069f39af08057
PeterSmart Link <petersmartlink@gmail.com>
2026-09-01 22:01:21 +0300

    Test matching Otya transfer tokens

--- test/transfer_resume_integrity_contract_test.dart
+++ test/transfer_resume_integrity_contract_test.dart
@@ -3,6 +3,16 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test('sender and receiver use the same 256-bit transfer token contract', () {
+    final sender = File('lib/features/transfer/data/media_sender.dart')
+        .readAsStringSync();
+    final receiver = File('lib/features/transfer/data/media_receiver.dart')
+        .readAsStringSync();
+
+    expect(sender, contains('List.generate(\n      32,'));
+    expect(receiver, contains("RegExp(r'^[a-f0-9]{64}\$')"));
+  });
+
   test('a 416 response is accepted only when local and remote lengths match', () {
     final source = File('lib/features/transfer/data/media_receiver.dart')
         .readAsStringSync();
