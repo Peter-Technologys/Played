f2b2b43cb686b219137855e473a56ea3e3584d29
PeterSmart Link <petersmartlink@gmail.com>
2026-09-03 13:21:55 +0300

    test(startup): align navigator context guards

--- test/startup_dialog_navigation_contract_test.dart
+++ test/startup_dialog_navigation_contract_test.dart
@@ -3,15 +3,27 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('startup dialogs use root navigator context and do not stack', () {
+  test('startup dialogs use fresh root navigator contexts and do not stack', () {
     final source = File('lib/app/app.dart').readAsStringSync();
 
     expect(source, contains('AppRouter.navigatorKey.currentContext'));
-    expect(source, contains('await AnnouncementDialog.showIfPending(navigatorContext);'));
-    expect(source, contains('await UpdateDialog.checkAndShow(navigatorContext);'));
     expect(
-      source.indexOf('await UpdateDialog.checkAndShow(navigatorContext);'),
-      greaterThan(source.indexOf('await AnnouncementDialog.showIfPending(navigatorContext);')),
+      source,
+      contains('await AnnouncementDialog.showIfPending(announcementContext);'),
+    );
+    expect(
+      source,
+      contains('await UpdateDialog.checkAndShow(updateContext);'),
+    );
+    expect(source, contains('!announcementContext.mounted'));
+    expect(source, contains('!updateContext.mounted'));
+    expect(
+      source.indexOf('await UpdateDialog.checkAndShow(updateContext);'),
+      greaterThan(
+        source.indexOf(
+          'await AnnouncementDialog.showIfPending(announcementContext);',
+        ),
+      ),
     );
     expect(source, isNot(contains('UpdateDialog.checkAndShow(context);')));
     expect(source, isNot(contains('AnnouncementDialog.showIfPending(context);')));
